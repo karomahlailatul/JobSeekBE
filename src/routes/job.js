bf546e9f35f29e5fff13ed0d4e6b72e6aac60911
Lailatul Karomah <karomahlailatul@gmail.com>
2022-10-04 08:22:40 +0700

    bugs job

--- src/routes/job.js
+++ src/routes/job.js
@@ -4,9 +4,9 @@ const jobController = require("../controller/job");
 const { protect } = require("../middlewares/auth");
 
 router.get("/", jobController.getPaginationJob);
+router.get("/:id", jobController.getJob);
 router.get("/fulldata", jobController.getPaginationJob_Recruiter_Skill);
 router.get("/fulldata/:id", jobController.getPaginationJob_Recruiter_Skill_ID);
-router.get("/:id", jobController.getJob);
 router.post("/", protect, jobController.insertJob);
 router.put("/:id", protect, jobController.updateJob);
 router.delete("/:id", protect, jobController.deleteJob);
